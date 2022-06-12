e590169c4831c2d0c2cc0a19bcc57eb2c62ee00e
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-06-12 16:12:16 +0100

    Adde caching

--- src/scripts/FunctionsLib.py
+++ src/scripts/FunctionsLib.py
@@ -1,6 +1,7 @@
 import numpy as np
 from functools import cache
 
+@cache
 def random_int_array(n_low=0, n_high=1000, n_vals = 100):
     '''
         Returns a numpy random integer array in the range(n_low, n_high) with array length n_vals.
