2ff7bb781951a9c377973dbc1e66294676e67623
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-06-14 20:57:46 +0100

    Updated setuo

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 data = dict(
-    name="src",
+    name="pyDev",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['pyDev*']),
 )
 
 if __name__ == '__main__':
